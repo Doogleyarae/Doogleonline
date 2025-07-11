a094a24f44b0040a2c3b6054d4b104732ef1a28d
Doogleyare <doogleyare3@gmail.com>
2025-07-11 23:27:05 +0300

    fix: Hide public navigation and footer on all /admin pages for privacy and security

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from "react";
-import { Switch, Route } from "wouter";
+import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -68,15 +68,17 @@ function Router() {
 }
 
 function App() {
+  const [location] = useLocation();
+  const isAdminRoute = location.startsWith("/admin");
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <div className="min-h-screen bg-gray-50 flex flex-col">
-          <Navigation />
+          {!isAdminRoute && <Navigation />}
           <main className="flex-1">
             <Router />
           </main>
-          <Footer />
+          {!isAdminRoute && <Footer />}
         </div>
         <Toaster />
       </TooltipProvider>
